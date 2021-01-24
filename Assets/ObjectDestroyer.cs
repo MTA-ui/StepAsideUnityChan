c7ebdd63341a2526f1bae58bbaac7c5d07eb1606
MTA-ui <73399838+MTA-ui@users.noreply.github.com>
2021-01-24 17:55:57 +0900

    Initial commit

---
+++ Assets/ObjectDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    private GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.camera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(camera.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
